c8aa5e88db31bb3d63350a84de0f2e8f0ca8184b
AdrianReinaB <adrian.reina.bellido.alu@iesjulioverne.es>
2024-05-12 23:08:04 +0200

    next commit

--- app/src/main/java/com/example/mapas/MapActivity.kt
+++ app/src/main/java/com/example/mapas/MapActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mapas
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.location.Location
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -13,6 +18,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.Task
@@ -45,7 +52,8 @@ class MapActivity : FragmentActivity(),  OnMapReadyCallback{
 
     }*/
 
-    lateinit var puntoActual:Location
+    //Mapa con tu localizacion
+    /*lateinit var puntoActual:Location
     lateinit var fusedClient:FusedLocationProviderClient
     companion object {
         private const val REQUEST_CODE = 101
@@ -102,5 +110,55 @@ class MapActivity : FragmentActivity(),  OnMapReadyCallback{
                 getLocation()
             }
         }
+    }*/
+
+    lateinit var gMap:GoogleMap
+    lateinit var map:FrameLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        map=findViewById(R.id.map)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+
+        gMap = p0
+
+        val sevilla = LatLng(37.3828300, -5.9731700)
+
+        //gMap.addMarker(MarkerOptions().position(sevilla).title("Está aquí"))
+        //gMap.moveCamera(CameraUpdateFactory.newLatLng(sevilla))
+
+        val marker=MarkerOptions().position(sevilla).title("Marker in Sydney")
+        //set custom icon
+        marker.icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_launcher_foreground))
+        //add marker
+        gMap.addMarker(marker)
+
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(sevilla))
+
+    }
+
+    private fun BitmapFromVector(context:Context,vectorResId:Int): BitmapDescriptor? {
+        //drawable generator
+        var vectorDrawable: Drawable
+        vectorDrawable= ContextCompat.getDrawable(context,vectorResId)!!
+        vectorDrawable.setBounds(0,0,vectorDrawable.intrinsicWidth,vectorDrawable.intrinsicHeight)
+        //bitmap genarator
+        var bitmap:Bitmap
+        bitmap= Bitmap.createBitmap(vectorDrawable.intrinsicWidth,vectorDrawable.intrinsicHeight,Bitmap.Config.ARGB_8888)
+        //canvas genaret
+        var canvas:Canvas
+        //pass bitmap in canvas constructor
+        canvas= Canvas(bitmap)
+        //pass canvas in drawable
+        vectorDrawable.draw(canvas)
+        //return BitmapDescriptorFactory
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
